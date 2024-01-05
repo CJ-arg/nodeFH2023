70dd836be2804f34f73d708d0aa4bf5186329a12
CJ-arg <janoncarlos@gmail.com>
2024-01-05 11:53:56 -0300

    ts refactor

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,12 +1,4 @@
-const { getUUID } = require('../plugins/get-id.plugin');
-const { getAge }  = require('../plugins/get-age.plugin');
-
-const { http } = require('../plugins/http-client.plugin');
-const buildLogger = require('../plugins/logger.plugin');
-
-module.exports = {
-  getAge, 
-  getUUID,
-  http,
-  buildLogger
-}
+export { getAge } from "./get-age.plugin";
+export { getUUID } from "./get-id.plugin";
+export { httpClientPlugin as httpClient } from "./http-client.plugin";
+export { buildLogger } from "./logger.plugin";
