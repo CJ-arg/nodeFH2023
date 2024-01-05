3ed9e3262d5c9c76b54daf2e5539857751c6a93f
CJ-arg <janoncarlos@gmail.com>
2024-01-05 11:56:38 -0300

    ts refactor

--- src/js-foundation/03-callbacks.ts
+++ src/js-foundation/03-callbacks.ts
@@ -1,28 +1,31 @@
-const users = [
+interface User {
+  id: number;
+  name: string;
+}
+
+const users: User[] = [
   {
     id: 1,
-    name: 'John Doe',
+    name: "John Doe",
   },
   {
     id: 2,
-    name: 'Jane Doe',
-  }
+    name: "Jane Doe",
+  },
 ];
 
-
-function getUserById( id, callback ) {
-  const user = users.find( function(user){
-    return user.id === id;  
+function getUserById(id: number, callback: any) {
+  const user = users.find(function (user) {
+    return user.id === id;
   });
 
-  if( !user ) {
+  if (!user) {
     return callback(`User not found with id ${id}`);
   }
 
-  return callback( null, user );
+  return callback(null, user);
 }
-
 
 module.exports = {
   getUserById,
-}
+};
