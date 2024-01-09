179d9302fbc91fd03d5f3949f825bbba6f56132d
CJ-arg <janoncarlos@gmail.com>
2024-01-09 10:30:44 -0300

    test

--- tests/js-foundation/01-template.test.ts
+++ tests/js-foundation/01-template.test.ts
@@ -4,4 +4,7 @@ describe("js-foundation/01-template", () => {
   test(" emailTemplate should contain a message", () => {
     expect(emailTemplate).toContain("Hi,");
   });
+  test("emailTemplete should contain  {{name}} and  {{orderId}}", () => {});
+  expect(emailTemplate).toMatch(/{{name}}/);
+  expect(emailTemplate).toMatch(/{{orderId}}/);
 });
