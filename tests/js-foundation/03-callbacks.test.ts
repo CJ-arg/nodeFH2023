4d98629c8056e4d906993f4d14f640af0c577db9
CJ-arg <janoncarlos@gmail.com>
2024-01-10 10:38:35 -0300

    test

---
+++ tests/js-foundation/03-callbacks.test.ts
@@ -0,0 +1,11 @@
+import { getUserById } from "../../src/js-foundation/03-callbacks";
+
+describe("ja-foundation/03-callbacks.ts", () => {
+  test("getUserById get an error user not found", () => {
+    const idTest = 10;
+
+    getUserById(idTest, (err, user) => {
+      expect(err).toBe(`User not found with id ${idTest}`);
+    });
+  });
+});
