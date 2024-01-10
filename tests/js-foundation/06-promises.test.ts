1ccc5ac52bff3c7bd4c8b6c7eafb28ea3de68929
CJ-arg <janoncarlos@gmail.com>
2024-01-10 15:40:17 -0300

    test

--- tests/js-foundation/06-promises.test.ts
+++ tests/js-foundation/06-promises.test.ts
@@ -1,1 +1,9 @@
 import { getPokemonById } from "../../src/js-foundation/06-promises";
+
+describe("js-foundation/06-promises.ts", () => {
+  test("getPokemonById should return a pokemon", async () => {
+    const pokemonID = 1;
+    const pokemonName = await getPokemonById(pokemonID);
+    expect(pokemonName).toBe("bulbasaur");
+  });
+});
