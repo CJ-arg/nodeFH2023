6cb1b5ab8c33acb63df76c0eca208c389e34a052
CJ-arg <janoncarlos@gmail.com>
2024-01-11 13:17:19 -0300

    test

--- tests/js-foundation/plugins/logger.plugin.test.ts
+++ tests/js-foundation/plugins/logger.plugin.test.ts
@@ -1,3 +1,10 @@
 import { buildLogger } from "../../../src/plugins";
 
-buildLogger;
+describe("logger.plugin.ts", () => {
+  test("buildLogger should return a function logger ", () => {
+    const logger = buildLogger("test");
+    // console.log(typeof logger.log);
+    expect(typeof logger.log).toBe("function");
+    expect(typeof logger.error).toBe("function");
+  });
+});
